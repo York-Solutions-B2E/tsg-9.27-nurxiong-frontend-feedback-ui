d40d54d99335d4a4af39455e5c9ab6330fac3cfb
leng03 <102699960+leng03@users.noreply.github.com>
2025-11-19 12:54:13 -0600

    catch error added

--- src/api/FeedbackAPI.ts
+++ src/api/FeedbackAPI.ts
@@ -1,7 +1,6 @@
-
 const baseURL = "http://localhost:8080/api/v1"
 
-export async function postFeedback(feedback :any, _fetch = fetch){
+export async function postFeedback(feedback: any, _fetch = fetch) {
     const data = await _fetch(baseURL + "/feedback", {
         method: "POST",
         headers: {
@@ -9,10 +8,18 @@ export async function postFeedback(feedback :any, _fetch = fetch){
         },
         body: JSON.stringify(feedback)
     })
+    if (!data.ok) {
+        const errorBody = await data.json()
+        throw new Error(errorBody.error);
+    }
     return await data.json()
 }
 
 export async function getFeedback(id: any, _fetch = fetch) {
     const data = await _fetch(baseURL + "/feedback?memberId=" + id)
+    if (!data.ok) {
+        const errorBody = await data.json()
+        throw new Error(errorBody.error);
+    }
     return await data.json()
 }
